d2c9a4608f9ee6ba4a2c9b28a9a1969f42c29374
TKGitEdu <tamdqse171713@fpt.edu.vn>
2026-06-06 13:28:53 +0700

    fix be for merge

--- backend/src/main/java/com/marinelink/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/marinelink/common/exception/GlobalExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import java.util.List;
 
 /**
