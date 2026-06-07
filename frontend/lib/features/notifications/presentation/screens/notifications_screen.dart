b1a0b549e2670a3ff4608ea1ae1ca4de8ee51dc9
TKGitEdu <tamdqse171713@fpt.edu.vn>
2026-06-07 09:51:16 +0700

    fix import needed

--- frontend/lib/features/notifications/presentation/screens/notifications_screen.dart
+++ frontend/lib/features/notifications/presentation/screens/notifications_screen.dart
@@ -1,8 +1,6 @@
 import 'package:go_router/go_router.dart';
 
 import '../../../../app/router/app_router.dart';
-import '../../../orders/presentation/screens/order_detail_screen.dart';
-import '../../../products/presentation/screens/product_detail_screen.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
